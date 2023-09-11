7b25f6f1249490f6fe30ac39f7d84fab00d33fc7
Kuzmin Roman <kuzmin.roman2002@yandex.ru>
2023-09-11 22:09:13 +0300

    API doc

--- LongevityInTimeTestTask/urls.py
+++ LongevityInTimeTestTask/urls.py
@@ -17,7 +17,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API doc for LIT test task",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny, ),
+)
+
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
     path('auth/', include('lit_auth.urls')),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
